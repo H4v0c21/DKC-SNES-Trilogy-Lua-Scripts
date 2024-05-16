af7d1a2286ff4e3c2a2dc11ec66fb00fb7ece83c
Alex Corley <alexcorley2011@hotmail.com>
2024-05-15 23:10:51 -0400

    Added DKC 3 Sprite Debug Script
    Added sprite debug script for DKC 3.
    Fixed a bug with hitbox flipping.

--- DKC 3 Sprite Debug (Mesen 2).lua
+++ DKC 3 Sprite Debug (Mesen 2).lua
@@ -1,5 +1,6 @@
---DKC 2 Sprite Debug Script v0.04
+﻿--DKC 3 Sprite Debug Script v0.04
 --By: H4v0c21
+--Adapted to DKC 3 by BlueImp
 --Designed for: Mesen S 2.0.0
 
 --Controls:
@@ -13,11 +14,11 @@
 
 mesen_version = 2
 
-object_base_address = 0xDE2
-object_size = 0x5E
-object_routine_table = 0xB38348
-animation_table = 0xF90000
-hitbox_table = 0xBCB600
+object_base_address = 0x878
+object_size = 0x6E
+object_routine_table = 0xB8A191
+animation_table = 0xF90100
+hitbox_table = 0xBC8000
 hitbox_base = 0xBC0000
 
 draw_object_info = true
@@ -27,18 +28,18 @@ freeze_objects = true
 hover_object_info = true
 
 draw_y_offset = 7
-bg_color = 0x80404040
+bg_color = 0x80606060
+
 
 if mesen_version == 2 then
 	ctrl_key = "Left Ctrl"
-	alt_key =  "Left Alt"
-	shift_key =  "Left Shift"
+	alt_key =	"Left Alt"
+	shift_key =	"Left Shift"
 else
 	ctrl_key = "Ctrl"
-	alt_key =  "Alt"
-	shift_key =  "Shift"
+	alt_key =	"Alt"
+	shift_key =	"Shift"
 end
-
 
 
 function ReadByte(address)
@@ -124,25 +125,24 @@ function IsMouseInsideBox(mouse_x, mouse_y, box_x, box_y, box_width, box_height)
 end
 
 
-function FindInTable(tbl, value)
-    
+function FindInTable(tbl, value)	
 	for i, entry in ipairs(tbl) do
 		v = entry[1]
-        if v == value then
-            return i
-        end
-    end
-    return nil
+		if v == value then
+			return i
+		end
+	end
+	return nil
 end
 
 
 colors = {
-	0xFF0000,	-- Red
 	0xFF00FF,	-- Magenta
+	0x00FFFF,	-- Cyan
 	0x00FF00,	-- Lime
 	0x0000FF,	-- Blue
 	0xFFFF00,	-- Yellow
-	0x00FFFF,	-- Cyan
+	0xFF0000,	-- Red
 	0xFF8000,	-- Orange
 	0x8000FF,	-- Purple
 	0x00FF80,	-- Spring green
@@ -163,7 +163,7 @@ colors = {
 	0xFF8C00	-- Dark orange
 }
 
-viewed_variable_number = 0
+viewed_variable_number = 00
 grabbed_objects = {}
 frozen_objects = {}
 
@@ -184,8 +184,7 @@ function Sprite_Debug()
 	
 	state = emu.getState()
 	mouseState = emu.getMouseState()
-	player_input = ReadWord(0x7E0510)
-	
+	player_input = ReadWord(0x7E04DA)
 	
 	--update input states
 	if mouse_left_hold ~= mouseState.left then
@@ -247,9 +246,9 @@ function Sprite_Debug()
 	if BitAND(player_input,0x0020) == 0x0020 then
 		viewed_variable_number = viewed_variable_number - 2
 		
-		--if variable underflowed loop back around to variable $5C,x
+		--if variable underflowed loop back around to variable $6C,x
 		if viewed_variable_number < 0 then
-			viewed_variable_number = 0x5C
+			viewed_variable_number = 0x6C
 		end
 	end
 	
@@ -258,7 +257,7 @@ function Sprite_Debug()
 		viewed_variable_number = viewed_variable_number + 2
 		
 		--if variable overflowed loop back around to variable $00,x
-		if viewed_variable_number >= 0x5E then
+		if viewed_variable_number >= 0x6E then
 			viewed_variable_number = 0
 		end
 	end
@@ -272,10 +271,10 @@ function Sprite_Debug()
 			gui_object_info_y_offset = 0
 			
 			--draw info box
-			emu.drawRectangle(0, 10, 110, 10, bg_color, true, 1)
+			emu.drawRectangle(0, 10, 85, 10, bg_color, true, 1)
 			
 			--draw info header text
-			emu.drawString(0, 10, "address type  $" .. ToHexByte(viewed_variable_number) .. ",x", 0xFFFFFF, 0xFF000000)
+			emu.drawString(0, 10, "Addr.  Id   " .. ToHexByte(viewed_variable_number) .. ",x", 0xFFFFFF, 0xFF000000)
 			
 			--update y draw offset
 			gui_object_info_y_offset = gui_object_info_y_offset + 20
@@ -283,8 +282,8 @@ function Sprite_Debug()
 	end
 	
 	--update game variables
-	camera_x = ReadWord(0x7E0AD7)
-	camera_y = ReadWord(0x7E0ADB)
+	camera_x = ReadWord(0x7E0493)
+	camera_y = ReadWord(0x7E0497)
 	
 	--iterate through objects
 	for object_index = 0,23,1 do
@@ -293,23 +292,26 @@ function Sprite_Debug()
 		object_color = colors[object_index+1]
 		
 		--get object info
-		object = object_size * object_index + object_base_address									--generate object address
-		object_type = ReadObjectVariableWord(object,0)												--get object type
-		object_main_routine = ReadObjectVariableWord(object_routine_table,object_type)
-		object_animation_id = ReadObjectVariableWord(object,0x36)
+		object = object_size * object_index + object_base_address
+		object_type = ReadObjectVariableWord(object,0)
+		object_main_routine = ReadObjectVariableWord(object,2)
+		object_main_routine = object_main_routine + (ReadObjectVariableByte(object,4) <<16)
+		object_animation_id = ReadObjectVariableWord(object,0x40)								 
+
+		--get animation id
 		object_animation_script = ReadObjectVariableWord(animation_table,object_animation_id*4)
 		
 		--if object is valid process the object
 		if object_type ~= 0 then
 			
 			--get object position and direction
-			object_x = ReadObjectVariableWord(object,0x6)
-			object_y = ReadObjectVariableWord(object,0xA)
-			object_facing = ReadObjectVariableWord(object,0x12)
+			object_x = ReadObjectVariableWord(object,0x12)
+			object_y = ReadObjectVariableWord(object,0x16)
+			object_facing = ReadObjectVariableWord(object,0x1E)
 			
 			--get object hitbox
-			hitbox_id = ReadObjectVariableWord(object,0x1A)
-			hitbox_offset = emu.readWord(hitbox_table+hitbox_id / 2, emu.memType[cpuDebug], false)
+			hitbox_id = ReadObjectVariableWord(object,0x22)
+			hitbox_offset = emu.readWord(hitbox_table+hitbox_id+3, emu.memType[cpuDebug], false)
 			hitbox_x_offset = emu.readWord(hitbox_base+hitbox_offset, emu.memType[cpuDebug], true)
 			hitbox_y_offset = emu.readWord(hitbox_base+hitbox_offset+2, emu.memType[cpuDebug], true)
 			hitbox_width = emu.readWord(hitbox_base+hitbox_offset+4, emu.memType[cpuDebug], true)
@@ -320,10 +322,10 @@ function Sprite_Debug()
 			
 			--if object is flipped mirror the hitbox
 			if BitAND(object_facing,0x4000) == 0x4000 then
-				hitbox_width = hitbox_width * -1
 				hitbox_x_offset = hitbox_x_offset * -1
+				hitbox_width = hitbox_width * -1
 			end
-	
+			
 			--render object hitbox
 			if draw_hitboxes == true then
 				emu.drawRectangle((object_x-camera_x)+hitbox_x_offset, (object_y-camera_y)+hitbox_y_offset+draw_y_offset, hitbox_width, hitbox_height, object_color+0x80000000, false, 1)
@@ -331,7 +333,7 @@ function Sprite_Debug()
 			
 			--render object info
 			if draw_object_info == true then
-				
+	
 				--if control isn't pressed render object info
 				if emu.isKeyPressed(ctrl_key) == false then
 					
@@ -339,10 +341,10 @@ function Sprite_Debug()
 					emu.drawPixel(object_x-camera_x, object_y-camera_y+draw_y_offset, object_color)
 					
 					--draw info box
-					emu.drawRectangle(0, gui_object_info_y_offset, 110, 10, bg_color, true, 1)
+					emu.drawRectangle(0, gui_object_info_y_offset, 85, 10, bg_color, true, 1)
 					
 					--draw info text
-					emu.drawString(0, gui_object_info_y_offset, "$" .. ToHexWord(object).. " #" .. ToHexWord(object_type) .." 0x" .. ToHexWord(viewed_variable), object_color, 0xFF000000)
+					emu.drawString(0, gui_object_info_y_offset, ToHexWord(object).. " " .. ToHexWord(object_type) .." " .. ToHexWord(viewed_variable), object_color, 0xFF000000)
 					
 					--update draw offset
 					gui_object_info_y_offset = gui_object_info_y_offset + 10
@@ -351,7 +353,7 @@ function Sprite_Debug()
 			
 			--handle object dragging
 			if drag_objects == true then
-				
+	
 				--if left click is pressed and if the mouse is over an object add it to dragged objects
 				if mouse_left_hold == true then
 					if IsMouseInsideBox(mouseState.x, mouseState.y, object_x-camera_x+hitbox_x_offset, object_y-camera_y+hitbox_y_offset+draw_y_offset, hitbox_width, hitbox_height) == true then
@@ -373,14 +375,14 @@ function Sprite_Debug()
 						else
 							table.remove(frozen_objects, table_index)
 						end
-
+			
 					end
 				end
 			end
 			
 			--handle object hover info
 			if hover_object_info == true then
-				
+	
 				--if mouse is over an object
 				if IsMouseInsideBox(mouseState.x, mouseState.y, object_x-camera_x+hitbox_x_offset, object_y-camera_y+hitbox_y_offset+draw_y_offset, hitbox_width, hitbox_height) == true then
 					
@@ -398,11 +400,11 @@ function Sprite_Debug()
 							emu.drawString(i * 14+14+4, 10, ToHexByte(i), object_color, 0xFF000000)
 						end
 						
-						for i = 0,5,1 do
+						for i = 0,6,1 do
 							emu.drawString(0, i * 16+24, ToHexByte(i), object_color, 0xFF000000)
 						end
 						
-						for v = 0,0x5D,1 do
+						for v = 0,0x6D,1 do
 							variable_byte = ReadObjectVariableByte(object,v)
 							emu.drawString((v%16) * 14 +14+4, (v//16) * 16+24, ToHexByte(variable_byte), object_color, 0x10000000)
 						end
@@ -415,7 +417,7 @@ function Sprite_Debug()
 						emu.drawString(mouseState.x, mouseState.y-draw_y_offset+10, "address: $" .. ToHexWord(object), object_color, 0xFF000000)
 						emu.drawString(mouseState.x, mouseState.y-draw_y_offset+20, "type: #" .. ToHexWord(object_type), object_color, 0xFF000000)
 						emu.drawString(mouseState.x, mouseState.y-draw_y_offset+30,"$" .. ToHexByte(viewed_variable_number) .. ",x: 0x" .. ToHexWord(viewed_variable), object_color, 0xFF000000)
-						emu.drawString(mouseState.x, mouseState.y-draw_y_offset+40, "main: $B3" .. ToHexWord(object_main_routine), object_color, 0xFF000000)
+						emu.drawString(mouseState.x, mouseState.y-draw_y_offset+40, "main: $" .. ToHexLong(object_main_routine), object_color, 0xFF000000)
 						emu.drawString(mouseState.x, mouseState.y-draw_y_offset+50, "anim: $F9" .. ToHexWord(object_animation_script), object_color, 0xFF000000)
 					end
 				end
@@ -427,8 +429,8 @@ function Sprite_Debug()
 	--move all dragged objects to mouse position
 	if drag_objects == true then
 		for object = 1, #grabbed_objects do
-			emu.writeWord(grabbed_objects[object]+0x06, mouseState.x+camera_x, emu.memType[cpuDebug])
-			emu.writeWord(grabbed_objects[object]+0x0A, mouseState.y-draw_y_offset+camera_y, emu.memType[cpuDebug])
+			emu.writeWord(grabbed_objects[object]+0x12, mouseState.x+camera_x, emu.memType[cpuDebug])
+			emu.writeWord(grabbed_objects[object]+0x16, mouseState.y-draw_y_offset+camera_y, emu.memType[cpuDebug])
 		end
 	end
 
@@ -438,8 +440,8 @@ function Sprite_Debug()
 			local object = frozen_object[1]
 			local x = frozen_object[2]
 			local y = frozen_object[3]
-			emu.writeWord(object+0x06, x, emu.memType[cpuDebug])
-			emu.writeWord(object+0x0A, y, emu.memType[cpuDebug])
+			emu.writeWord(object+0x12, x, emu.memType[cpuDebug])
+			emu.writeWord(object+0x16, y, emu.memType[cpuDebug])
 			emu.drawRectangle(x-camera_x-4, y-camera_y+draw_y_offset-4, 2, 8, 0x00FFFFFF, true, 1)
 			emu.drawRectangle(x-camera_x, y-camera_y+draw_y_offset-4, 2, 8, 0x00FFFFFF, true, 1)
 		end
@@ -451,4 +453,4 @@ end
 emu.addEventCallback(Sprite_Debug, emu.eventType.endFrame);
 
 --display startup message
-emu.displayMessage("Script", "DKC 2 Sprite Debug Script loaded.")
+emu.displayMessage("Script", "DKC 3 Sprite Debug Script loaded.")
